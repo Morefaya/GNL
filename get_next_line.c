7ba15c997be701d029f4c1dcf25efd2cbbf19fd3
Jean-charles CAZAKO <jcazako@e1r6p7.42.fr>
2016-02-01 20:59:02 +0100

    2nd retry

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: jcazako <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/01/27 13:39:32 by jcazako           #+#    #+#             */
-/*   Updated: 2016/01/31 17:12:43 by jcazako          ###   ########.fr       */
+/*   Updated: 2016/02/01 20:37:50 by jcazako          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,9 +37,10 @@ static t_gnl		*rd_list(t_list **list, const int fd)
 	ft_lstadd(list, lst_tmp);
 	return ((*list)->content);
 }
+
 static int			gnl(char **line, char **str)
 {
-	int			ret;
+	int				ret;
 	char			*tmp;
 
 	ret = 0;
@@ -70,28 +71,30 @@ static int			read_fd(t_gnl **content)
 	char			*tmp;
 	int				ret;
 
-	ft_bzero(buff, BUFF_SIZE + 1);
-	while ((ret = read((*content)->fd, buff, BUFF_SIZE)) > 0)
+	while (!ft_strchr((*content)->str, '\n'))
 	{
+		ft_bzero(buff, BUFF_SIZE + 1);
+		if ((ret = read((*content)->fd, buff, BUFF_SIZE)) == -1)
+			return (-1);
+		if (!ret)
+			return (0);
 		tmp = (*content)->str;
 		if (!((*content)->str = ft_strjoin((*content)->str, buff)))
-			return (1);
-		ft_bzero(buff, BUFF_SIZE + 1);
+			return (-1);
 		free(tmp);
 	}
-	if (ret == -1)
-		return (1);
-	return (0);
+	return (1);
 }
 
 int					get_next_line(const int fd, char **line)
 {
 	static t_list	*list;
 	t_gnl			*gnl_data;
+	int				ret;
 
 	if (BUFF_SIZE <= 0 || !line || !(gnl_data = rd_list(&list, fd)))
 		return (-1);
-	if (read_fd(&gnl_data))
+	if ((ret = read_fd(&gnl_data)) == -1)
 		return (-1);
 	return (gnl(line, &(gnl_data->str)));
 }
